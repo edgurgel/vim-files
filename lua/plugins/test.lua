a51f4d76f14d4fc4fc2c7d0b70891aeb55ad8c07
edgurgel <eduardo@gurgel.me>
2024-06-23 20:48:11 +1200

    Updates

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -8,8 +8,10 @@ return {
     "jfpedroza/neotest-elixir",
   },
   keys = {
-    { '<leader>tr', "<cmd>lua require('neotest').run.run()<CR>",                   remap = false, desc = "Run nearby test" },
-    { '<leader>tf', "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", remap = false, desc = "Run test file" },
+    { '<leader>tr', "<cmd>lua require('neotest').run.run()<CR>",                     remap = false, desc = "Run nearby test" },
+    { '<leader>tf', "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>",   remap = false, desc = "Run test file" },
+    { '<leader>td', "<cmd>lua require('neotest').output.open({ enter = true })<CR>", remap = false, desc = "Test diagnostics" },
+    { '<leader>tp', "<cmd>lua require('neotest').output_panel.toggle()<CR>", remap = false, desc = "Test panel" },
   },
   config = function()
     require("neotest").setup({
