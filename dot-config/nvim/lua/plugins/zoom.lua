cada69d9051786890e11c6be6d34f895e57ff1bf
Eduardo Gurgel Pinho <eduardo.gurgelpinho@3plearning.com>
2024-07-04 19:40:22 +1200

    Ajust focus to have a minwidth

--- dot-config/nvim/lua/plugins/zoom.lua
+++ dot-config/nvim/lua/plugins/zoom.lua
@@ -2,7 +2,22 @@ return {
   'nvim-focus/focus.nvim',
   version = '*',
   config = function()
-    require("focus").setup()
+    require("focus").setup({
+      enable = true,          -- Enable module
+      commands = true,        -- Create Focus commands
+      autoresize = {
+        enable = true,        -- Enable or disable auto-resizing of splits
+        width = 0,            -- Force width for the focused window
+        height = 0,           -- Force height for the focused window
+        minwidth = 10,        -- Force minimum width for the unfocused window
+        minheight = 0,        -- Force minimum height for the unfocused window
+        height_quickfix = 10, -- Set the height of quickfix panel
+      },
+      split = {
+        bufnew = false, -- Create blank buffer for new split windows
+        tmux = false,   -- Create tmux splits instead of neovim splits
+      },
+    })
     local ignore_filetypes = { 'NvimTree', 'toggleterm' }
     local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
 
