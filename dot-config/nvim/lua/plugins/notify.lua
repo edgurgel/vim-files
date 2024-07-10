4e6578cb6436a53f8ac4754d123bc2eb9b93d9af
edgurgel <eduardo@gurgel.me>
2024-07-10 22:29:53 +1200

    Bottom notifications

--- dot-config/nvim/lua/plugins/notify.lua
+++ dot-config/nvim/lua/plugins/notify.lua
@@ -1,6 +1,13 @@
 return {
   'rcarriga/nvim-notify',
-  config = function()
+  opts = {
+    timeout = 3000,
+    render = "compact",
+    stages = "fade",
+    top_down = false,
+  },
+  config = function(_, opts)
     vim.notify = require("notify")
+    require('notify').setup(opts)
   end
 }
