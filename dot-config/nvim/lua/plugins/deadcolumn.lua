f9e909fbfb77dd9896fc02cb5007c51a59a6f68d
Eduardo Gurgel Pinho <eduardo.gurgelpinho@3plearning.com>
2024-07-18 16:03:11 +1200

    Move colorcolumn into deadcolumn setup

--- dot-config/nvim/lua/plugins/deadcolumn.lua
+++ dot-config/nvim/lua/plugins/deadcolumn.lua
@@ -3,6 +3,8 @@ return {
   version = '*',
   event = "VeryLazy",
   config = function()
+    vim.opt.colorcolumn = '120'
+
     require('deadcolumn').setup({
       modes = { 'n', 'i' },
       scope = 'visible'
