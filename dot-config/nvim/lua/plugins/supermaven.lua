1bf380a5a45bf7682004cef3a1e44715163cddc5
Eduardo Gurgel Pinho <eduardo.gurgelpinho@3plearning.com>
2025-01-15 14:10:25 +1300

    Update kitty  config and dsisable inline supermaven

--- dot-config/nvim/lua/plugins/supermaven.lua
+++ dot-config/nvim/lua/plugins/supermaven.lua
@@ -3,7 +3,7 @@ return {
     "supermaven-inc/supermaven-nvim",
     config = function()
       require("supermaven-nvim").setup({
-        disable_inline_completion = false, -- disables inline completion for use with cmp
+        disable_inline_completion = true, -- disables inline completion for use with cmp
         disable_keymaps = false,
       })
     end,
