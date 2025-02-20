ae7e4cdcd8a1e1d128a130b41c187a3a3df4976d
Eduardo Gurgel Pinho <eduardo.gurgelpinho@3plearning.com>
2025-02-21 10:39:34 +1300

    Add lsp lines and treewalker

--- dot-config/nvim/lua/plugins/lspconfig.lua
+++ dot-config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,14 @@
 return {
   {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    config = function()
+      require("lsp_lines").setup()
+      vim.diagnostic.config({
+        virtual_text = false,
+      })
+    end,
+  },
+  {
     'neovim/nvim-lspconfig',
     version = "*",
     dependencies = {
--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,29 @@
 return {
   {
+    'aaronik/treewalker.nvim',
+    event = { "BufReadPost", "BufNewFile" },
+    keys = {
+      -- { "<C-k>", "<cmd>Treewalker Up<cr>",    desc = "TreeWalker Up",    silent = true, mode = { 'n', 'v' } },
+      -- { "<C-j>", "<cmd>Treewalker Down<cr>",  desc = "TreeWalker Down",  silent = true, mode = { 'n', 'v' } },
+      -- { "<C-h>", "<cmd>Treewalker Left<cr>",  desc = "TreeWalker Left",  silent = true, mode = { 'n', 'v' } },
+      -- { "<C-l>", "<cmd>Treewalker Right<cr>", desc = "TreeWalker Right", silent = true, mode = { 'n', 'v' } },
+    },
+    -- The following options are the defaults.
+    -- Treewalker aims for sane defaults, so these are each individually optional,
+    -- and setup() does not need to be called, so the whole opts block is optional as well.
+    opts = {
+      -- Whether to briefly highlight the node after jumping to it
+      highlight = true,
+
+      -- How long should above highlight last (in ms)
+      highlight_duration = 250,
+
+      -- The color of the above highlight. Must be a valid vim highlight group.
+      -- (see :h highlight-group for options)
+      highlight_group = 'CursorLine',
+    }
+  },
+  {
     'Wansmer/treesj',
     -- dir = "~/dev/treesj",
     event = { "BufReadPost", "BufNewFile" },
