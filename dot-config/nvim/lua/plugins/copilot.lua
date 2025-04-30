b8abd1289045f3da4f62327a266d382c00c4d754
Eduardo Gurgel Pinho <eduardo@gurgel.me>
2025-05-01 09:53:45 +1200

    Update copilotchat and set skitch and spotify to float

--- dot-config/nvim/lua/plugins/copilot.lua
+++ dot-config/nvim/lua/plugins/copilot.lua
@@ -14,7 +14,7 @@ return {
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
+    version = "*",
     dependencies = {
       "zbirenbaum/copilot.lua",
       "nvim-lua/plenary.nvim", -- For curl, log wrapper
