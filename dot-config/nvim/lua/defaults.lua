095b200b6a5f6c4e6ddb6c0a896592ca5627617b
Eduardo Gurgel Pinho <eduardo@gurgel.me>
2025-06-07 20:20:48 +1200

    lsp update

--- dot-config/nvim/lua/defaults.lua
+++ dot-config/nvim/lua/defaults.lua
@@ -65,3 +65,14 @@ vim.filetype.add({
     ['http'] = 'http',
   },
 })
+-- inline diagnostics
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
